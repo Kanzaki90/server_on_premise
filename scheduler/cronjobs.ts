ba795a3e804e024ba0bb0ac1847b02b46d071939
Kanzaki90 <antonhanin7@gmail.com>
2024-05-12 13:19:10 +0300

    files altered

--- scheduler/cronjobs.ts
+++ scheduler/cronjobs.ts
@@ -6,7 +6,13 @@ import Bree from "bree";
 export const BreeLauncher = (): void => {
     console.info(` - ${new Date()} | Bree Has Been launched`);
 
-    const _path = path.join(__dirname, "../", "jobs", "backupLauncher.ts");
+    let _path;
+    if (process.env.IS_DEV && process.env.IS_DEV === "true") {
+        _path = path.join(__dirname, "../", "jobs", "backupLauncher.ts");
+    } else {
+        _path = path.join(__dirname, "../dist", "jobs", "backupLauncher.js");
+
+    }
     const bree = new Bree({
         logger: new Cabim(),
         root: path.join(__dirname, "../", "jobs"),
